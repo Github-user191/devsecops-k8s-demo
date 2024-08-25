e2ed0a2574ce912755cc99a700bcc3e6ac742b2a
Github-user191 <luqmaan786921@gmail.com>
2024-08-25 10:33:10 +0200

    add stage - kubsec

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#kubesec-scan.sh
-
-# using kubesec v2 api
+# using kubesec v2 api to scan our manifest file
+# jq is a JSON processor to grab the response of the scan and decide to pass or fail the build
 scan_result=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
-scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r ) 
-scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score ) 
+scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r )
+scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score )
 
 
 # using kubesec docker image for scanning
@@ -13,9 +12,6 @@ scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https:/
 # scan_message=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml | jq .[].message -r)
 # scan_score=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml | jq .[].score)
 
-	
-    # Kubesec scan result processing
-    # echo "Scan Score : $scan_score"
 
 	if [[ "${scan_score}" -ge 5 ]]; then
 	    echo "Score is $scan_score"

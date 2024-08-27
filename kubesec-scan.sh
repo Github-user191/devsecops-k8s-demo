451e336b78e4a00cf85879aeb943376ded6c8c7f
luqmaan-onsight <138872917+luqmaan-onsight@users.noreply.github.com>
2024-08-27 20:25:44 +0200

    ee333

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -2,9 +2,9 @@
 
 # using kubesec v2 api to scan our manifest file
 # jq is a JSON processor to grab the response of the scan and decide to pass or fail the build
-scan_result=$(curl -sSX POST --data-binary @"k8s_DEV-deployment_service.yaml" https://v2.kubesec.io/scan)
-scan_message=$(curl -sSX POST --data-binary @"k8s_DEV-deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r )
-scan_score=$(curl -sSX POST --data-binary @"k8s_DEV-deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score )
+scan_result=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
+scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r )
+scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score )
 
 
 # using kubesec docker image for scanning

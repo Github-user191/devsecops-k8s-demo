78060263f3c3da4637015c3cf3962f9103aeae37
luqmaan-onsight <138872917+luqmaan-onsight@users.noreply.github.com>
2024-08-27 20:33:22 +0200

    ee3332

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
+
+#k8s-deployment-rollout-status.sh
+
 sleep 60s
 
-# Usage: ./k8s-deployment-rollout-status.sh <namespace>
-
-# Check if the correct number of arguments are provided
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <namespace>"
-  exit 1
-fi
-
-# Assign arguments to variables
-namespace=$1
-
-if [[ $(kubectl -n "${namespace}" rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]];
+if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]];
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
-    kubectl -n "${namespace}" rollout undo deploy ${deploymentName}
+    kubectl -n default rollout undo deploy ${deploymentName}
     exit 1;
 else
 	echo "Deployment ${deploymentName} Rollout is Success"
